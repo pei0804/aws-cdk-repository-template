02fe838f30134d721bed64f3ca90256b18bf165c
pei0804 <peeeei0804@gmail.com>
2021-03-14 13:49:58 +0900

    init

--- stacks/bin/cdk.ts
+++ stacks/bin/cdk.ts
@@ -9,10 +9,11 @@ import { EcrStack } from "../lib/ecr";
   const branchName = env("BRANCH_NAME");
   const name = `cdk-template-${branchName}`;
   const stackPrefix = branchName === "main" ? "prod" : "dev";
+
   const stackProps = await NewStackProps({
     env: {
-      account: env("CDK_DEFAULT_ACCOUNT"),
-      region: env("CDK_DEFAULT_REGION"),
+      account: process.env.CDK_DEFAULT_ACCOUNT,
+      region: process.env.CDK_DEFAULT_REGION,
     },
     tags: {
       Name: name,
@@ -21,6 +22,7 @@ import { EcrStack } from "../lib/ecr";
 
   const app = new cdk.App();
   new EcrStack(app, `${stackPrefix}-ecr-${branchName}`, stackProps);
+
   app.synth();
 })().catch((e) => {
   console.error(e);
